ed5c9982bbcfcb2db0f9b48987f3d6a0584a6983
cdc-001 <cory.caddell@gmail.com>
2023-02-11 16:47:11 -0600

    add get population sum and display to screen

--- CoryCaddellAssignment1C.java
+++ CoryCaddellAssignment1C.java
@@ -9,9 +9,8 @@ public class CoryCaddellAssignment1C {
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		Map<String, Integer> CoryCaddellStructure = new TreeMap<>(); 
-
 		
-		// Read data from file
+	// Read data from file
 		// Check to see if file exists
 		File file = new File("Assignment1CData.txt");
 		if (!file.exists())
@@ -19,24 +18,35 @@ public class CoryCaddellAssignment1C {
 			System.out.print("File does not exist.");
 			System.exit(0);
 		}
-		// While file has next line, read line of text and store in input variable
+		// While file has next line
 		Scanner input = new Scanner(file);
 		while(input.hasNext()) {
+			// Read line of text
 			String dataPerLine = input.nextLine();
-			String[] mapEntry = dataPerLine.split("[, ]");
-			String key = mapEntry[mapEntry.length -1];
-			Integer value = Integer.valueOf(mapEntry[mapEntry.length - 2]);
-			CoryCaddellStructure.put(key, value);
+			// Split out words from input variable
+			String[] mapEntry = dataPerLine.split("[, 	]");
+			// Second to last element will be key in Map
+			String key = mapEntry[mapEntry.length - 2];
+			// Last element will be value in Map
+			Integer value = Integer.valueOf(mapEntry[mapEntry.length - 1]);
+			
+	// Get total population per state
+			// If key is already present in Map
+			if (CoryCaddellStructure.containsKey(key))
+			{
+				// Increase value by current population value
+				CoryCaddellStructure.put(key, CoryCaddellStructure.get(key) + value);
+			}
+			else {
+				// Add entry to data structure
+				CoryCaddellStructure.put(key, value);
+			}
 		}
 		input.close();
-		// Split out words from input variable, and store in a Map data structure
-			// Ignore text before comma, comma, and white spaces
-			// First word after comma will be key in Map
-			// Number following key will be value in Map
-				// Convert number to an integer
-	// Sum total population per state
-		// If key is already present in Map, increase value by current population value
+		
 	// Display population sum per state to screen in table format
+		System.out.printf("%-20s%s\n", "State", "Total Population");
+		CoryCaddellStructure.forEach((key, value) -> System.out.printf("%-20s%,d\n", key, value));
 	}
 
 }
