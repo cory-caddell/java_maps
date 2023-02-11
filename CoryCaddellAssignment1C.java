cb09a20e5a9153d5c0c4c579f082c7d751934580
cdc-001 <cory.caddell@gmail.com>
2023-02-11 14:22:08 -0600

    add pseudocode

--- CoryCaddellAssignment1C.java
+++ CoryCaddellAssignment1C.java
@@ -1,10 +1,24 @@
 //CoryCaddellAssignment1C.java
 
+import java.util.*;
+import java.io.*;
 
 public class CoryCaddellAssignment1C {
 	
-	public static void main(String[] args) {
+	/** Program stores and display total population per state. */
+	public static void main(String[] args) throws FileNotFoundException {
 		
+		// Read data from file
+			// Check to see if file exists, and display error message if it doesn't
+			// While file has next line, read line of text and store in input variable
+			// Split out words from input variable, and store in a Map data structure
+				// Ignore text before comma, comma, and white spaces
+				// First word after comma will be key in Map
+				// Number following key will be value in Map
+					// Convert number to an integer
+		// Sum total population per state
+			// If key is already present in Map, increase value by current population value
+		// Display population sum per state to screen in table format
 	}
 
 }
